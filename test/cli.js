c4f4c1c0e00b3c243e197f3591c9a660ec3847b9
Raul Ochoa <rochoaf@gmail.com>
2016-12-16 16:52:20 +0000

    Avoid running native tests with node < 4.0.0

--- test/cli.js
+++ test/cli.js
@@ -1,10 +1,14 @@
 var ConnectionParameters = require(__dirname + '/../lib/connection-parameters');
 var config = new ConnectionParameters(process.argv[2]);
+var semver = require('semver')
 
 for(var i = 0; i < process.argv.length; i++) {
   switch(process.argv[i].toLowerCase()) {
   case 'native':
-    config.native = true;
+    if (semver.gte(process.version, '4.0.0')) {
+      console.log('Not running native in node < v4.0.0')
+      config.native = true;
+    }
     break;
   case 'binary':
     config.binary = true;
